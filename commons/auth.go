c45bcd205917c5340be804338570002c1e157316
Marcos Pizzio <marcos.pizzio@mercadolibre.com>
2019-02-19 11:42:41 -0300

    Add generación de token

---
+++ commons/auth.go
@@ -0,0 +1,58 @@
+package commons
+
+import (
+	"crypto/rsa"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/mpizziomeli/proyectoGo/proyectoGo/models"
+	"io/ioutil"
+	"log"
+	"time"
+)
+
+var (
+	privateKey *rsa.PrivateKey
+	PublicKey *rsa.PublicKey
+)
+
+func init()  {
+	privateBytes, err := ioutil.ReadFile("./keys/private.rsa")
+	if err != nil {
+		log.Fatal("No se pudo leer la private key")
+	}
+
+	publicBytes, err := ioutil.ReadFile("./keys/public.rsa")
+	if err != nil {
+		log.Fatal("No se pudo leer la private key")
+	}
+
+	privateKey, err = jwt.ParseRSAPrivateKeyFromPEM(privateBytes)
+	if err != nil {
+		log.Fatal("No se pudo parsear la private key")
+	}
+
+	PublicKey, err = jwt.ParseRSAPublicKeyFromPEM(publicBytes)
+	if err != nil {
+		log.Fatal("No se pudo parsear la public key")
+	}
+
+
+
+}
+
+func GenerateJWT(user models.User) string  {
+	claims := models.Claim{
+		User: user,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt:time.Now().Add(time.Hour *2).Unix(),
+			Issuer: "Marcos John Golang",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	result, err := token.SignedString(privateKey)
+	if err != nil {
+		log.Fatal("No se pudo firmar el token")
+	}
+
+	return result
+}
